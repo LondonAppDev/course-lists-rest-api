852fc850a24fe72c2406d317604940c7bc9fd1dd
Mark Winterbottom <mark@londonappdeveloper.com>
2017-02-21 16:31:12 +0000

    Added views file contents.

--- src/list_project/list_api/views.py
+++ src/list_project/list_api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import serializers, viewsets
 
-# Create your views here.
+from . import models
+
+
+class ListItemSerializer(serializers.ModelSerializer):
+    """
+    Serializer for converting JSON strings to a database object and
+    for converting a database object to JSON string.
+    """
+
+    class Meta:
+        model = models.ListItem
+        fields = ('id', 'name', 'date_created',)
+        read_only_fields = ('id', 'date_created',)
+
+
+class ListItemViewSet(viewsets.ModelViewSet):
+    """
+    Viewset for managing list items via HTTP requests.
+    """
+
+    serializer_class = ListItemSerializer
+    queryset = ListItem.objects.all()
